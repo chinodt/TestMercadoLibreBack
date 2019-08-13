85d8d827d3fdff3ed03a4932b560940645e1bf37
David Tineo <david.tineo@grupo-vs.com>
2019-08-13 06:38:39 -0400

    Endpoint de Mercadolibre

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// ./src/index.js
+const express = require('express')
+const app = express()
+const port = 3000
+// require API_helper.js
+const api_helper = require('./API_helper')
+
+app.get('/', (req, res) => res.send('Hola Mercado Libre!'))
+
+app.get('/getProduct/:product', (req, res) => {
+    var product = req.params.product;
+    api_helper.make_API_call('https://api.mercadolibre.com/sites/MLA/search?q=' + product)
+    .then(response => {
+        res.json(response)
+    })
+    .catch(error => {
+        res.send(error)
+    })
+})
+
+app.get('/getProductById/:id', (req, res) => {
+    var id = req.params.id;
+    api_helper.make_API_call('https://api.mercadolibre.com/items/' + id)
+    .then(response => {
+        res.json(response)
+    })
+    .catch(error => {
+        res.send(error)
+    })
+})
+
+app.get('/getProductDescription/:id', (req, res) => {
+    var id = req.params.id;
+    api_helper.make_API_call('https://api.mercadolibre.com/items/' + id + '/description')
+    .then(response => {
+        res.json(response)
+    })
+    .catch(error => {
+        res.send(error)
+    })
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
